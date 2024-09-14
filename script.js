8234ded16eff1023f77a0aa3001b09d2cd3ee759
syntaxglow <romanov.t1@mail.ru>
2024-09-14 12:02:11 +0300

    update script.js

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 const div = document.querySelector('.count-js');
 const buttonCount = document.querySelector('.button-js-count');
 const buttonClear = document.querySelector('.button-js-clear')
-div.style.cssText = `
-	text-align:center;
-	width:20px;
-	height:20px;
-	background: red;
-	padding: 10px 15px;
-	margin: 5px 0 5px 0;
-	color: white;
-	font-size: 20px;
-`
-
 let count = 0;
+div.textContent = count;
 
 buttonCount.addEventListener('click', () => {
 	countBtn()
